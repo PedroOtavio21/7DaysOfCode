a3962482cc2f33e3bde0ce68eb4775eccb65b163
Pedro Otávio <pedrootavio.bezerra14@gmail.com>
2024-11-26 10:58:42 -0300

    Adiciona solução extra em javascript e visualização de resultado no html do dia 2 em 7DaysOfCode

--- day2/index.js
+++ day2/index.js
@@ -14,3 +14,26 @@ const idade = parseInt(prompt("Quantos anos você tem?"))
 const linguagem = prompt("Qual linguagem de programação você está estudando?")
 
 alert(`Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`)
+
+/** Exercício Opcional
+ * Você vai complementar o código para que, depois de exibir a mensagem anterior, o programa pergunte:
+ * Você gosta de estudar [linguagem]? Responda com o número 1 para SIM ou 2 para NÃO.
+ * 
+ * E aí, dependendo da resposta, ele deve mostrar uma das seguintes mensagens:
+ * 1 > Muito bom! Continue estudando e você terá muito sucesso. 
+ * 2 > Ahh que pena... Já tentou aprender outras linguagens?
+*/
+
+let opniao = prompt(`Você gosta de estudar ${linguagem}? Responda 1 para "Sim" e 2 para "Não"`)
+
+while(opniao !== "1" && opniao !== "2"){
+    opniao = prompt("Opção inserida inválida. Tente novamente.")
+}
+
+if(opniao === "1"){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.")
+}
+
+if(opniao === "2"){
+    alert("Ahh que pena... Já tentou aprender outras linguagens?")
+}
